43a467f6bf1bf6114642e576cd24ac5077e45f20
Anastasia <an.afalin@yandex.ru>
2022-10-03 10:45:38 +0300

    add class "JDBCConnection" in "JDBC-Homework"

---
+++ JDBC-Homework/src/main/java/JDBCConnection.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/db_test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection(){
+        return connection;
+    }
+}
